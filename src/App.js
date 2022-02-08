01c29ca6d90622d9f97a67a486129752f69c7add
DLongz <mrlewis1188@gmail.com>
2022-02-08 05:16:26 -0500

    PokeDex Complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=2000')
+        .then(response => response.json())
+        .then(response => setPokemon(response.results))
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pokem Poke-Dex</h1>
+      {pokemon.length > 0 && pokemon.map((poke, index) => {
+        return(
+          <div key={index}>
+              <li>{poke.name}</li>
+          </div>
+        )
+      })}
     </div>
   );
 }
